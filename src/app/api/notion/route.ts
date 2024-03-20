64fbaba1e31ad5f78c24142d900181f447c34252
monichre <liamhellis@gmail.com>
2024-03-19 23:04:44 -0500

    fixing route handler

--- src/app/api/notion/route.ts
+++ src/app/api/notion/route.ts
@@ -5,31 +5,38 @@ export async function POST(request: any) {
   console.log('request: ', request)
   try {
     const req = await request.json()
-    const {
-      body: {
-        payload: { page },
-      },
-    } = req
-    console.log('page: ', page)
-
-    const event = client.sendEvent({
-      id: 'notion-create-page',
-      name: 'notion-create-page',
-      payload: {
-        page: {
-          page,
+    console.log('req: ', req)
+    if (req?.body || req?.payload) {
+      const {
+        body: {
+          payload: { page },
         },
-      },
-    })
-    console.log('event: ', event)
+      } = req
+      console.log('page: ', page)
 
-    // Example: Send a response
-    return Response.json({
-      status: 200,
-      event,
-    })
+      const event = client.sendEvent({
+        id: 'notion-create-page',
+        name: 'notion-create-page',
+        payload: {
+          page: {
+            page,
+          },
+        },
+      })
+      console.log('event: ', event)
+
+      // Example: Send a response
+      return new Response.json({
+        status: 200,
+        event,
+      })
+    }
     // Process the webhook payload
   } catch (error: any) {
     console.log('error: ', error)
+    return new Response.json({
+      status: 404,
+      message: 'Error',
+    })
   }
 }
